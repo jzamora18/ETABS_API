65fdf75dc2e4686463418a82e5b56d566e9eae40
Juan José <juanjosezamoraj@hotmail.com>
2025-11-06 11:47:23 -0500

    Fix order and torsional irregularity

--- scr/utils.py
+++ scr/utils.py
@@ -3,20 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def coef_import(GrupoUso):
-    if GrupoUso == "I":
+def coef_import(use_group):
+    if use_group == "I":
         return 1.0
-    elif GrupoUso == "II":
+    elif use_group == "II":
         return 1.1
-    elif GrupoUso == "III":
+    elif use_group == "III":
         return 1.25
-    elif GrupoUso == "IV":
+    elif use_group == "IV":
         return 1.5
     else:
-        raise ValueError("Grupo de uso no válido")
+        raise ValueError("Group of use no valid")
 
 
-def plot_espectro_nsr10(
+def plot_spectrum(
     Aa,
     Av,
     Fa,
@@ -24,25 +24,24 @@ def plot_espectro_nsr10(
     T0,
     Tc,
     Tl,
-    CoefImportancia,
+    importance_factor,
     R,
-    analisis_dinamico=False,
-    tipo_espectro="diseño",
+    spectrum_type="design",
 ):
     T = np.linspace(0, 8, 500)
     Sa = np.zeros_like(T)
     Sa1 = np.full_like(T, np.nan)  # Use NaN for no values
 
-    # Espectro de Diseño NSR-10
+    # Design spectrum calculation based on NSR-10
     for i, t in enumerate(T):
         if t < Tc:
-            Sa[i] = 2.5 * Aa * Fa * CoefImportancia
+            Sa[i] = 2.5 * Aa * Fa * importance_factor
         elif t < Tl:
-            Sa[i] = 1.2 * Av * Fv * CoefImportancia / t
+            Sa[i] = 1.2 * Av * Fv * importance_factor / t
         else:
-            Sa[i] = 1.2 * Av * Fv * Tl * CoefImportancia / t**2
+            Sa[i] = 1.2 * Av * Fv * Tl * importance_factor / t**2
 
-    plt.plot(T, Sa / R, label=f"Espectro de {tipo_espectro}")
+    plt.plot(T, Sa / R, label=f"Spectrum type: {spectrum_type}")
 
     return T, Sa
 
